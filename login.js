736c6cc8596fff068aa880a2cc2c92111776c089
sarahmib <sarahmibeecroft@gmail.comgit config --global user.name sarahmib>
2023-12-12 18:43:27 -0700

    login authorization

--- login.js
+++ login.js
@@ -1,5 +1,33 @@
-function login() {
-    const uname = document.querySelector("#name");
-    localStorage.setItem("username", uname.value);
+async function login() {
+  const userName = document.querySelector('#name')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch('/api/auth/login', {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem(userName, uname.value);
     window.location.href = "calendar.html";
   }
+  }
+
+async function createUser() {
+  const userName = document.querySelector('#name')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch('/api/auth/create', {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem(userName, uname.value);
+    window.location.href = "calendar.html";
+  }
+}
